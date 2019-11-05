c53cf7ab939e707ecd3d59290b93774c6656d280
mjb <mbrown49@qub.ac.uk>
2019-11-05 17:12:42 +0000

    Began work on getopts implementation We now process arguments. Also suitable functions have been written (pending filling). Further separated into suitable headers. Added figlet-generated logo just for fun.

--- functfun/fftest.c
+++ functfun/fftest.c
@@ -8,41 +8,124 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include<getopt.h>
+#include<unistd.h>
 
+#include"ffglobals.h"
 #include"fftest.h"
 
-#define DEFAULT_BOUND 200000000
-#define PRINT_BOUND_LIMIT 10000
+// mode definitions
+#define READ_IN 1
+#define WRITE_OUT 2
+#define GENERATE_ONLY 3
 
-int readLogTable(FILE *logFile);
-int writeLogTable(FILE *logFile);
-int generateLogs(float *logTable, int upperBound);
-int printLogTable(float *logTable, int upperBound);
-int printLogTableToCon(float *logTable, int upperBound);
+int readInLogFile(char *fileName, float *logTable, int upperBound);
+int writeOutLogFile(char *fileName, float* logTable, int upperBound);
+int generateLogsOnly(float *logTable, int upperBound);
 
 int main(int argc, char** argv){
-    // Initially assume 2G to be filled, later have getopts
-    // but while we test let's just keep that low 
+    char *logo="   ________          __\n  / _/ _/ /____ ___ / /_\n / _/ _/ __/ -_|_-</ __/\n/_//_/ \\__/\\__/___/\\__/\n";
+    char *banner=" FFTEST: Fun Function Tests. Flood your RAM for fun! \n=====================================================\n\
+ Usage: ... Note: Only the first arg of ... is honoured. -b is optional.";
 
-    absoluteUint maxBound=4*8*100000; // what do you
+    int upperBound=0;
 
-    int upperBound=DEFAULT_BOUND;
+    /* handle getopts */
+    opterr=1;
+    int thisArg;
+    int mode=0;
+    char *fileName = NULL;
+
+    if(argc>3){
+        fprintf(stderr, "Invalid call; too many arguments!\n%s\n", banner);
+        exit(1);
+    }
+
+    while ((thisArg=getopt(argc, argv, "hr:w:gb:")) != -1){
+        switch(thisArg){
+            case'r':
+                fileName=optarg;
+                printf("Reading log file %s...\n", fileName);
+                mode=READ_IN; 
+                break;
+            case'w':
+                fileName=optarg;
+                printf("Logs will be written to log file %s...\n", fileName);
+                mode=WRITE_OUT;
+                break;
+            case'g':
+                puts("Call to generate only. No logfile will be used.");
+                mode=GENERATE_ONLY;
+                break;
+            case'b':
+                upperBound=atoi(optarg);
+                break;
+            // print help banner in these cases
+            case'h':
+                printf("%s\n%s\n",logo, banner);
+                exit(0);
+            case'?':
+                printf("%s\n", banner);
+                exit(1);
+            default:
+                abort(); break;
+        }
+    }
+    
+    if (mode==0){
+        puts("Mode unset, assuming generate only.");
+        mode=GENERATE_ONLY;
+    }
+
+    /* getopts complete, carry on */
+    
+    // This should handle unset and bad input to -b
+    if (upperBound==0){
+        printf("Total logs to generate is unset. Using default value of %d.\n", DEFAULT_BOUND);
+        upperBound=DEFAULT_BOUND;
+    }
+    
     // pointer to allocate array in heap memory
     float *logTable = malloc(upperBound* sizeof(float));
-    generateLogs(logTable, upperBound);
-    printLogTableToCon(logTable, upperBound);
 
-    printf("generation of %d logs complete... ctrl+c or ENTER to quit.\n", upperBound);
-    getchar();
+    // Now do the work based on what was selected.
+    switch (mode) {
+        case READ_IN:
+           readInLogFile(fileName, logTable, upperBound);
+           break;
+        case WRITE_OUT:
+           writeOutLogFile(fileName, logTable, upperBound);
+           break;
+        case GENERATE_ONLY:
+           generateLogsOnly(logTable, upperBound);
+           break;
+    }
 
     free(logTable); // may need to handle sigterm to ensure this is freed
 
     return 0;
 }
 
+int generateLogsOnly(float *logTable, int upperBound){
+    generateLogs(logTable, upperBound);
+    printLogTableToCon(logTable, upperBound);
+
+    printf("generation of %d logs complete... ctrl+c or ENTER to quit.\n", upperBound);
+    getchar();
+    return 0;
+}
+
+int readInLogFile(char *fileName, float *logTable, int upperBound){
+    puts("not yet ready sorry sir");
+    return 0;
+}
+
+int writeOutLogFile(char *fileName, float* logTable, int upperBound){
+    puts("also not yet ready, sorry again");
+    return 0;
+}
+
 int printLogTable(float *logTable, int upperBound){
-    // print result TODO: better format
+    // print result TODO: better format; this goes well beyond 80cols
     for(int i=0; i<upperBound; i++){
         printf("ln(%d): %3.4f\t", i, logTable[i]);
         if (i%10==0) puts(""); //newline
--- functfun/fftest.h
+++ functfun/fftest.h
@@ -1,2 +1,13 @@
-/* global header for typedefs */
-typedef unsigned long int absoluteUint; 
+/*
+ * Function headers for fftest.c
+ * Note: These functions are only for fftest itself!
+ * MJB 05 11 2019 
+ */
+#define DEFAULT_BOUND 200000000
+#define PRINT_BOUND_LIMIT 10000
+
+int readLogTable(FILE *logFile);
+int writeLogTable(FILE *logFile);
+int generateLogs(float *logTable, int upperBound);
+int printLogTable(float *logTable, int upperBound);
+int printLogTableToCon(float *logTable, int upperBound);
