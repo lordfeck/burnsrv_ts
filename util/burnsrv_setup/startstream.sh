07e1a0269e4689d244d1f5fb1cea6e6f2bbe7adb
mjb <mbrown49@qub.ac.uk>
2019-12-09 11:01:37 +0000

    added format to startstream script

--- util/burnsrv_setup/startstream.sh
+++ util/burnsrv_setup/startstream.sh
@@ -5,15 +5,16 @@
 
 . stream.config
 
-readonly banner="START STREAM SCRIPT\nUsage:\n-i <stream input>
--o Stream server output\n-h show help
+readonly banner="START STREAM SCRIPT\nUsage:\n-i <stream input file>
+-o <Stream server output URL>\n-e <encoding>\n-h show help
 Running with no input will use the settings in stream.config."
 
 # Handle arguments
-while getopts ":i:h" opt; do
+while getopts ":i:ho:e:" opt; do
     case $opt in
         i ) stream="$OPTARG" ;;
         o ) server="$OPTARG" ;;
+        e ) encoding="$OPTARG" ;;
         h ) echo -e "$banner"; exit 0 ;;
         \? ) echo -e "Invalid Argument.\n$banner"; exit 1 ;;
     esac
@@ -30,5 +31,10 @@ if [ -z $server ]; then
     server="$defaultServer"
 fi
 
+if [ -z $encoding ]; then
+    echo "Using configured server $defaultEncoding."
+    encoding="$defaultEncoding"
+fi
+
 # Begin stream
-ffmpeg -i $stream -f flv $server 
+ffmpeg -i $stream -f $encoding $server 
