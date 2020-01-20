fb59b9a7eb658b95030a78f8c674000f0557e78e
mjb <mbrown49@qub.ac.uk>
2020-01-20 21:48:28 +0000

    For issue #2: Began work on ffcmp

---
+++ functfun/ffcmp.c
@@ -0,0 +1,13 @@
+/* FFcmp; read in binfiles and calculate no. of errors
+ * Authored: 20/01/2020
+ *
+ */
+
+#include<stdio.h>
+
+int main ()
+{
+    char *banner = "FFCmp: Comparison and Conversion utility.";
+    printf("%s\n", banner);
+    return 0;
+}
