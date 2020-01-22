bca8dc7689f4a74bcfcbd8332cd1b30c7008f2df
mjb <mbrown49@qub.ac.uk>
2020-01-22 15:22:25 +0000

    Extended ffcmp to have a calculate base diff function (#10) It just cycles through both arrays. If either entry at the same position is a mismatch, then the base diff is incremented. Extend to include error rate, support more than two binfiles, have option of printing each difference to cmd line or file?

--- functfun/ffcmp.c
+++ functfun/ffcmp.c
@@ -10,6 +10,9 @@
 #include"fflib.h"
 #include"ffcmp.h"
 
+float calculateErrorRate(int baseDiff, int upperBound);
+int calculateBaseDiff(float *logTable1, float *logTable2, int upperBound);
+
 int main (int argc, char** argv)
 {
     char *banner = "FFCmp: Binary comparison utility.";
@@ -21,15 +24,41 @@ int main (int argc, char** argv)
     // count them up and allocate.
     float *logTable1, *logTable2;
     int upperBound1, upperBound2;
+    int baseDiff;
+
+    printf("Argc: %d\n", argc);
 
     if (argc <2) {
         puts("Two arguments required. Go and have two rows with a political opposite.");
         exit(1);
     }
 
+    // temp measure, make filename pointer to arg
+    filename1=argv[1];
+    filename2=argv[2];
+
     puts("Reading in file 1...");
     readInBin(filename1, &logTable1, &upperBound1);
     printf("Read in %d logs from %s.\n", upperBound1, filename1);
+    readInBin(filename2, &logTable2, &upperBound2);
+    printf("Read in %d logs from %s.\n", upperBound1, filename1);
+
+    if (upperBound1!=upperBound2) {
+        fprintf(stderr, "Mismatch between log boundaries %d!=%d. (NOT YET) Using the lesser for both.\n", upperBound1, upperBound2);
+        exit(1);
+    }
+
+    baseDiff=calculateBaseDiff(logTable1, logTable2, upperBound1);
+    printf("Calculated base diff: %d \n", baseDiff);
 
     return 0;
 }
+
+int calculateBaseDiff(float *logTable1, float *logTable2, int upperBound) {
+    int baseDiff=0;
+    for (int i=0; i<upperBound; i++) {
+        if(logTable1[i]!=logTable2[i]) baseDiff++;
+    }
+    return baseDiff;
+}
+
--- functfun/fflib.c
+++ functfun/fflib.c
@@ -9,7 +9,15 @@
 
 int readInBin(const char *fileName, float **logTable, int *upperBound){
     FILE *binFile=fopen(fileName, "rb");
-    
+
+    #ifdef DEBUG
+    if (binFile!=NULL) {
+        fprintf(stderr, "File: %s successfully opened.\n", fileName);
+    } else {
+        fprintf(stderr, "Problem opening the file %s.\n", fileName);
+    }
+    #endif
+   
     // read size in integer then allocate log table
     fread(upperBound, sizeof(int), 1, binFile);
     #ifdef DEBUG
