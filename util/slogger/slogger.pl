ece93b4c3c3d10b3e18e313b852c7b2dd15ddd4e
mjb <mbrown49@qub.ac.uk>
2020-04-08 19:52:35 +0100

    Issue #16: Implement analysis of captures Slogger now opens server list csv. It doesn't yet do more, but in time it shall.

--- util/slogger/slogger.pl
+++ util/slogger/slogger.pl
@@ -1,3 +1,29 @@
 #!/usr/bin/perl
 
-print "Work is underway...\n";
+# Slogger: Slog through your logs
+# Author: MJB Authored: 08/04/2020
+
+use warnings;
+use feature ':5.10';
+
+package slogger;
+
+$srvListPath="../testcfg/servers.csv";
+
+say "Welcome to slogger!";
+
+open (SRVLIST, "<", "$srvListPath") or die "Critical! Failed to open server list CSV!\n";
+
+# First read them in
+while ($line = <SRVLIST>) {
+    ($hostname, $shortdesc, $longdesc) = split(",", $line);
+    $servers{$shortdesc} = $hostname;
+    $longDescs{$shortdesc} = $longdesc;
+}
+
+# Read them back to be sure it worked. (not quite right yet)
+foreach $hostname (%servers) {
+    say "$hostname";
+}
+
+close (SRVLIST);
