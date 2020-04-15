fcacc1e9ca3575ae154404bb725ef69516823d9d
mjb <mbrown49@qub.ac.uk>
2020-04-15 19:25:28 +0100

    Issue #21 Let testrun.sh read in the servers.csv file Added the ability to read in the server list csv. Issue #16 Implement analysis of captures Also added stream time recording.

--- util/testcfg/testrunner.sh
+++ util/testcfg/testrunner.sh
@@ -9,21 +9,40 @@ if [ ! -s "stream.config" ]; then
 fi
 
 . stream.config
+declare -a streamServers
+
+# how to read in csv to bash, src: https://stackoverflow.com/a/4286841/11937530
+function readInServers {
+    if [ ! -s "$1" ]; then
+        echo "Critical failure: $1 server list doesn't exist or is empty. Exiting."
+        exit 1
+    fi
+
+    let i=0
+    while IFS=, read -r hostname shortdesc longdesc
+    do
+        streamServers[$i]="$hostname"
+        ((i++))
+    done < "$1"
+}
 
 function checkError {
-if [ "$?" -ne "0" ]; then
-    echo "CRITICAL: Error when $1. Exiting."
-    exit 1
-fi
+    if [ "$?" -ne "0" ]; then
+        echo "CRITICAL: Error when $1. Exiting."
+        exit 1
+    fi
 }
 
 function checkWarning {
-if [ "$?" -ne "0" ]; then
-    echo "WARNING: Failure when $1. Please check the configuration!"
-fi
+    if [ "$?" -ne "0" ]; then
+        echo "WARNING: Failure when $1. Please check the configuration!"
+    fi
 }
 
 # BEGIN PREPARATION SECTION
+
+readInServers "$serverList"
+
 # If Cap dir doesn't exist, make it.
 if [ ! -d "${capDir}/prev/" ]; then
     mkdir -p "${capDir}/prev/"
@@ -38,25 +57,16 @@ for server in "${streamServers[@]}"; do
 done
 echo ""
 
-#echo -n "These are: "
-#for desc in "${streamServersDesc[@]}"; do
-#    echo -n "$desc, "
-#done
-#echo ""
-
 # Test for local presence of tcpdump
 if ! which tcpdump >/dev/null; then
     echo "Tcpdump is not installed. Cannot continue." 1>&2
     exit 1
 fi
 
-# Sync time of all servers.
+# NTP sync time of all servers.
 if [ "$skipTestSync" != "true" ]; then
     ./greenwich.sh -s
 fi
-
-# get start time
-startTime="$( date +'%M %S %N' )"
 
 # END PREP
 
@@ -65,7 +75,8 @@ startTime="$( date +'%M %S %N' )"
 echo "Launching tcpdump and performing stream on each specified server."
 
 # Backup previous captures
-find ${capDir}/* -maxdepth 0 -type f -exec mv -f -v {} ${capDir}/prev/ \;
+2>&1 find ${capDir}/* -maxdepth 0 -type f -exec mv -f -v {} ${capDir}/prev/ \; >/dev/null
+checkWarning "backing up old captures"
 
 for server in "${streamServers[@]}"; do
     scp -q "starttcpdump.sh" "${userName}@${server}:/home/$userName/"
@@ -74,18 +85,29 @@ for server in "${streamServers[@]}"; do
     checkError "Beginning remote tcpdump for $server."
     ./starttcpdump.sh "$localIface" "./$capDir/$server.local"
     checkError "Beginning local tcpdump for ${server}"
-    echo "Beginning stream in 4..."
+    echo "Beginning stream for $server length $streamMaxLength in 4..."
     sleep 4
+
+    # %s - seconds since unix epoch, %N is nanoseconds
+    before="$( date '+%s%N' )"
     ./startclient.sh -v -n 1 -s "rtmp://${server}:${rtmpPort}/${rtmpPath}/${streamFiles[0]}.${streamFormat}" -t "$streamMaxLength"
-    checkError "Beginning ${server}'s stream."
+    after="$( date '+%s%N' )"
+    # checkError "Beginning ${server}'s stream."
     echo "Stream over, killing remote and local tcpdump."
     sleep 4
+
     ssh "${userName}@${server}" 'sudo kill -2 `pgrep tcpdump`'
     checkWarning "Killing remote TCPdump for $server"
     sudo kill -2 `pgrep tcpdump` 2>&1 >/dev/null
     checkWarning "Killing local TCPdump for $server"
-    # remove any output from local tcpd run (uncomment to debug)
+    # remove any output from local tcpd run (comment out to debug)
     #rm -vf "tcpderror.log"
+
+    runtime="$((after-before))"
+    ms="$((runtime/1000000))"
+    echo $runtime > "${capDir}/${server}.runtime"
+    echo "Stream ran for $runtime ns, i.e. $ms ms"
+
 done
 
 echo "Correcting ownership inside $capDir recursively."
@@ -95,7 +117,7 @@ sudo chown -R $localUser $capDir
 
 # BEGIN HOUSEKEEPING & ANALYSIS
 # Retrieve tcpdump captures & remove old captures 
-echo "Retrieving and/or removing=$removeOldCap old captures..."
+echo "Retrieving ${removeOldCap:+'and removing'} old captures on the remote hosts..."
 for server in "${streamServers[@]}"; do
     scp "${userName}@${server}:/home/$userName/$pcapFileName" "./$capDir/$server.remote"
     if [ "$removeOldCap" = "true" ]; then
